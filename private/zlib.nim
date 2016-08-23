2bfaa0654d7b5ad77fcb6959df25756d99efa8b0
Silvio T <stisa4@gmail.com>
2016-08-23 23:29:34 +0200

    add license to zlib

--- private/zlib.nim
+++ private/zlib.nim
@@ -1,6 +1,30 @@
 # Converted from Pascal
 
 ## Interface to the zlib http://www.zlib.net/ compression library.
+# Taken from https://github.com/nim-lang/zip/blob/master/zip/zlib.nim
+# License: https://github.com/nim-lang/zip/blob/master/LICENSE
+#[
+  The MIT License (MIT)
+  Copyright (c) 2015 
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+]#
 
 when defined(windows):
   const libz = "zlib1.dll"
